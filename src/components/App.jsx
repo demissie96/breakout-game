0f55395cb3eb80479fd5374d9edecbc5b4b4f333
Johannes Demissie <demissie.johannes@gmail.com>
2022-09-07 15:08:55 +0200

    Make the ball bounce back from the borders.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
+import "./App.css";
 
 var ball;
 var paddle;
+const leftBorder = 0;
+const rightBorder = 780;
+const topBorder = 0;
+const bottomBorder = 580;
 
 function App() {
   // Define a state variable to store and access the fabric.Canvas object
@@ -15,6 +20,7 @@ function App() {
       width: 800,
       backgroundColor: "grey",
       borderScaleFactor: 3,
+      centeredScaling: true,
     });
   // Set global border styles on select
   fabric.Object.prototype.set({
@@ -38,47 +44,77 @@ function App() {
     ball = new fabric.Circle({
       radius: 10,
       fill: "red",
-      left: 400,
-      top: 500,
+      left: 390,
+      top: 530,
+      selectable: false,
     });
     paddle = new fabric.Rect({
       height: 20,
       width: 150,
       fill: "blue",
-      left: 400,
+      left: 325,
       top: 550,
+      selectable: false,
     });
     canvas.add(ball, paddle);
   }
 
   function StartBall() {
     var pixelMove = 3;
-    var direction = true;
+    var directionUp = true;
+    var directionDown = false;
+    var directionLeft = true;
+    var directionRight = false;
     setInterval(() => {
-      if (ball.top > 200 && direction === true) {
+      if (directionUp === true && directionLeft === true && ball.left > leftBorder && ball.top > topBorder) {
         ball.top -= pixelMove;
         ball.left -= pixelMove;
         canvas.renderAll();
         console.log(ball.top);
-      } else if (ball.top < 500 && direction === false) {
-        direction = false;
-        console.log(direction);
+      } else if (directionDown === true && directionRight === true && ball.left < rightBorder && ball.top < bottomBorder ) {
         ball.top += pixelMove;
         ball.left += pixelMove;
         canvas.renderAll();
         console.log(ball.top);
-      } else {
-        direction = !direction;
+      } else if (directionDown === true && directionLeft === true && ball.left > leftBorder && ball.top < bottomBorder ) {
+        ball.top += pixelMove;
+        ball.left -= pixelMove;
+        canvas.renderAll();
+        console.log(ball.top);
+      }  else if (directionUp === true && directionRight === true && ball.left < rightBorder && ball.top > topBorder ) {
+        ball.top -= pixelMove;
+        ball.left += pixelMove;
+        canvas.renderAll();
+        console.log(ball.top);
+      }  else {
+        if(ball.left <= leftBorder) {
+          directionLeft = false;
+          directionRight = true;
+          console.log("direction right activated");
+        } else if (ball.left >= rightBorder){
+          directionLeft = true;
+          directionRight = false;
+          console.log("direction left activated");
+        } else if (ball.top <= topBorder){
+          directionUp = false;
+          directionDown = true;
+          console.log("direction down activated");
+        } else if (ball.top >= bottomBorder){
+          directionUp = true;
+          directionDown = false;
+          console.log("direction up activated");
+        }       
       }
     }, 20);
   }
 
   return (
     <>
-      <div>
+      <div style={{ textAlign: "center" }}>
         <button onClick={() => InitialBallAndPaddle()}>Start Game</button>
         <button onClick={() => StartBall()}>Start Ball</button>
-        <canvas id="canvas" width="200px" height="200px"></canvas>
+
+        <canvas id="canvas" width="800px" height="600px"></canvas>
       </div>
     </>
   );
